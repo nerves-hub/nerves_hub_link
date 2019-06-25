a7f778ab037ff309939ea7208e89dce721deb90f
Jon Carstens <jjcarstens@me.com>
2019-06-25 12:10:53 -0600

    Don't link to HTTPFwupStream process
    Resolves https://github.com/nerves-hub/nerves_hub/issues/95
    
    Changes the connection to HTTPFwupStream to just `start` it instead of linking the process. With the link, if the HTTPFwup stream crashes, the whole Channel genserver also restarts which forces it to rejoin NervesHub, creating a duplicate Phoenix channel that needs to be cleaned up. This lets us instead keep the channel open and all state with it instead of needlessly restarting.

--- lib/nerves_hub_device/channel.ex
+++ lib/nerves_hub_device/channel.ex
@@ -119,7 +119,7 @@ defmodule NervesHubDevice.Channel do
     # to control exactly when an update is applied.
     case Client.update_available(@client, data) do
       :apply ->
-        {:ok, http} = HTTPFwupStream.start_link(self())
+        {:ok, http} = HTTPFwupStream.start(self())
         spawn_monitor(HTTPFwupStream, :get, [http, url])
         Logger.info("[NervesHubDevice] Downloading firmware: #{url}")
         state
