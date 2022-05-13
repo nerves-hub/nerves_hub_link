e9a85c73e324b2033fd96f6a8a067a5222b8a716
Jon Carstens <jjcarstens@me.com>
2022-05-13 07:38:17 -0600

    v1.2.0 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesHubLink.MixProject do
   use Mix.Project
 
-  @version "1.1.0"
+  @version "1.2.0"
   @source_url "https://github.com/nerves-hub/nerves_hub_link"
 
   Application.put_env(
