addd2d03f709d1ac54b0461f54e81d80c23709f4
Jon Carstens <jjcarstens@me.com>
2022-08-05 11:46:00 -0600

    bump nerves_runtime 0.13.1

--- config/dev.exs
+++ config/dev.exs
@@ -25,24 +25,22 @@ key =
   if File.exists?("./nerves-hub/test-key.pem"),
     do: File.read!("./nerves-hub/test-key.pem")
 
-config :nerves_runtime, :kernel, autoload_modules: false
-config :nerves_runtime, target: "host"
-
-config :nerves_runtime, Nerves.Runtime.KV.Mock, %{
-  "nerves_fw_active" => "a",
-  "a.nerves_fw_uuid" => "8a8b902c-d1a9-58aa-6111-04ab57c2f2a8",
-  "a.nerves_fw_product" => "nerves_hub",
-  "a.nerves_fw_architecture" => "x86_64",
-  "a.nerves_fw_version" => "0.1.0",
-  "a.nerves_fw_platform" => "x86_84",
-  "a.nerves_fw_misc" => "extra comments",
-  "a.nerves_fw_description" => "test firmware",
-  "nerves_hub_cert" => cert,
-  "nerves_hub_key" => key,
-  "nerves_fw_devpath" => "/tmp/fwup_bogus_path",
-  "nerves_serial_number" => "test"
-}
-
-config :nerves_runtime, :modules, [
-  {Nerves.Runtime.KV, Nerves.Runtime.KV.Mock}
-]
+config :nerves_runtime,
+  target: "host",
+  kernel: [autoload_modules: false],
+  kv_backend:
+    {Nerves.Runtime.KVBackend.InMemory,
+     contents: %{
+       "nerves_fw_active" => "a",
+       "a.nerves_fw_uuid" => "8a8b902c-d1a9-58aa-6111-04ab57c2f2a8",
+       "a.nerves_fw_product" => "nerves_hub",
+       "a.nerves_fw_architecture" => "x86_64",
+       "a.nerves_fw_version" => "0.1.0",
+       "a.nerves_fw_platform" => "x86_84",
+       "a.nerves_fw_misc" => "extra comments",
+       "a.nerves_fw_description" => "test firmware",
+       "nerves_hub_cert" => cert,
+       "nerves_hub_key" => key,
+       "nerves_fw_devpath" => "/tmp/fwup_bogus_path",
+       "nerves_serial_number" => "test"
+     }}
--- config/test.exs
+++ config/test.exs
@@ -27,24 +27,22 @@ config :nerves_hub_link,
   rejoin_after: 0,
   remote_iex: true
 
-config :nerves_runtime, :kernel, autoload_modules: false
-config :nerves_runtime, target: "host"
-
-config :nerves_runtime, Nerves.Runtime.KV.Mock, %{
-  "nerves_fw_active" => "a",
-  "a.nerves_fw_uuid" => "8a8b902c-d1a9-58aa-6111-04ab57c2f2a8",
-  "a.nerves_fw_product" => "nerves_hub",
-  "a.nerves_fw_architecture" => "x86_64",
-  "a.nerves_fw_version" => "0.1.0",
-  "a.nerves_fw_platform" => "x86_84",
-  "a.nerves_fw_misc" => "extra comments",
-  "a.nerves_fw_description" => "test firmware",
-  "nerves_hub_cert" => "cert",
-  "nerves_hub_key" => "key",
-  "nerves_fw_devpath" => "/tmp/fwup_bogus_path",
-  "nerves_serial_number" => "test"
-}
-
-config :nerves_runtime, :modules, [
-  {Nerves.Runtime.KV, Nerves.Runtime.KV.Mock}
-]
+config :nerves_runtime,
+  target: "host",
+  kernel: [autoload_modules: false],
+  kv_backend:
+    {Nerves.Runtime.KVBackend.InMemory,
+     contents: %{
+       "nerves_fw_active" => "a",
+       "a.nerves_fw_uuid" => "8a8b902c-d1a9-58aa-6111-04ab57c2f2a8",
+       "a.nerves_fw_product" => "nerves_hub",
+       "a.nerves_fw_architecture" => "x86_64",
+       "a.nerves_fw_version" => "0.1.0",
+       "a.nerves_fw_platform" => "x86_84",
+       "a.nerves_fw_misc" => "extra comments",
+       "a.nerves_fw_description" => "test firmware",
+       "nerves_hub_cert" => "cert",
+       "nerves_hub_key" => "key",
+       "nerves_fw_devpath" => "/tmp/fwup_bogus_path",
+       "nerves_serial_number" => "test"
+     }}
