ae2ab258023f62482cd368fcb46636d81f2cc076
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-04-18 13:51:44 +1200

    tidy up a few logging messages

--- lib/nerves_hub_link/configurator.ex
+++ lib/nerves_hub_link/configurator.ex
@@ -132,9 +132,11 @@ defmodule NervesHubLink.Configurator do
     fwup_public_keys = for key <- config.fwup_public_keys, is_binary(key), do: key
 
     if fwup_public_keys == [] do
-      Logger.error("No fwup public keys were configured for nerves_hub_link.")
-      Logger.error("This means that firmware signatures are not being checked.")
-      Logger.error("nerves_hub_link will fail to apply firmware updates.")
+      Logger.warning("""
+      No fwup public keys were configured for nerves_hub_link.
+      This means that firmware signatures are not being checked.
+      nerves_hub_link will fail to apply firmware updates.
+      """)
     end
 
     %{config | fwup_public_keys: fwup_public_keys}
@@ -144,9 +146,11 @@ defmodule NervesHubLink.Configurator do
     archive_public_keys = for key <- config.archive_public_keys, is_binary(key), do: key
 
     if archive_public_keys == [] do
-      Logger.error("No archive public keys were configured for nerves_hub_link.")
-      Logger.error("This means that archive signatures are not being checked.")
-      Logger.error("nerves_hub_link will fail to download archives.")
+      Logger.debug("""
+      No archive public keys were configured for nerves_hub_link.
+      This means that archive signatures are not being checked.
+      nerves_hub_link will fail to download archives.
+      """)
     end
 
     %{config | archive_public_keys: archive_public_keys}
