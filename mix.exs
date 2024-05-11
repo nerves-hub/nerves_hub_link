4275c649077f57908522e8f7ecede4bd1ca94f81
Eric Oestrich <eric@oestrich.org>
2024-05-11 11:37:10 -0400

    v2.3.0 release (#186)
    * v2.3.0 release
    
    * CI: bump 1.12 and 1.16 to connect to hex

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesHubLink.MixProject do
   use Mix.Project
 
-  @version "2.2.1"
+  @version "2.3.0"
   @description "Manage your Nerves fleet by connecting it to NervesHub"
   @source_url "https://github.com/nerves-hub/nerves_hub_link"
 
