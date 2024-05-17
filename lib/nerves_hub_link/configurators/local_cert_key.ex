0c995b40e2e76b1bb4d6d74131888787deec11d2
Josh Kalderimis <josh.kalderimis@gmail.com>
2024-05-17 15:28:00 +1200

    use the `config.data_path` for the default cert and key

--- lib/nerves_hub_link/configurators/local_cert_key.ex
+++ lib/nerves_hub_link/configurators/local_cert_key.ex
@@ -8,51 +8,55 @@ defmodule NervesHubLink.Configurator.LocalCertKey do
 
   @impl NervesHubLink.Configurator
   def build(%Config{} = config) do
+    config
+    |> maybe_add_cacerts()
+    |> maybe_add_cert()
+    |> maybe_add_key()
+    |> maybe_add_sni()
+  end
+
+  defp maybe_add_cacerts(%{ssl: ssl} = config) do
+    %{config | ssl: Keyword.put_new(ssl, :cacerts, Certificate.ca_certs())}
+  end
+
+  defp maybe_add_cert(%{ssl: ssl} = config) do
     ssl =
-      config.ssl
-      |> maybe_add_cacerts()
-      |> maybe_add_cert()
-      |> maybe_add_key()
-      |> maybe_add_sni(config)
+      cond do
+        ssl[:cert] || ssl[:certfile] ->
+          ssl
+
+        cert = Nerves.Runtime.KV.get(@cert_kv_path) ->
+          cert = Certificate.pem_to_der(cert)
+
+          Keyword.put(ssl, :cert, cert)
+
+        true ->
+          Keyword.put_new(ssl, :certfile, Path.join(config.data_path, "cert.pem"))
+      end
 
     %{config | ssl: ssl}
   end
 
-  defp maybe_add_cacerts(socket_opts) do
-    Keyword.put_new(socket_opts, :cacerts, Certificate.ca_certs())
+  defp maybe_add_key(%{ssl: ssl} = config) do
+    ssl =
+      cond do
+        ssl[:key] || ssl[:keyfile] ->
+          ssl
+
+        key = Nerves.Runtime.KV.get(@key_kv_path) ->
+          key = Certificate.key_pem_to_der(key)
+
+          Keyword.put(ssl, :key, {:ECPrivateKey, key})
+
+        true ->
+          Keyword.put_new(ssl, :keyfile, Path.join(config.data_path, "key.pem"))
+      end
+
+    %{config | ssl: ssl}
   end
 
-  defp maybe_add_cert(socket_opts) do
-    cond do
-      socket_opts[:cert] || socket_opts[:certfile] ->
-        socket_opts
-
-      cert = Nerves.Runtime.KV.get(@cert_kv_path) ->
-        cert = Certificate.pem_to_der(cert)
-
-        Keyword.put(socket_opts, :cert, cert)
-
-      true ->
-        Keyword.put_new(socket_opts, :certfile, "/data/nerves_hub/cert.pem")
-    end
-  end
-
-  defp maybe_add_key(socket_opts) do
-    cond do
-      socket_opts[:key] || socket_opts[:keyfile] ->
-        socket_opts
-
-      key = Nerves.Runtime.KV.get(@key_kv_path) ->
-        key = Certificate.key_pem_to_der(key)
-
-        Keyword.put(socket_opts, :key, {:ECPrivateKey, key})
-
-      true ->
-        Keyword.put_new(socket_opts, :keyfile, "/data/nerves_hub/key.pem")
-    end
-  end
-
-  defp maybe_add_sni(socket_opts, %{device_api_sni: sni}) do
-    Keyword.put_new(socket_opts, :server_name_indication, to_charlist(sni))
+  defp maybe_add_sni(%{ssl: ssl, device_api_sni: sni} = config) do
+    ssl = Keyword.put_new(ssl, :server_name_indication, to_charlist(sni))
+    %{config | ssl: ssl}
   end
 end
