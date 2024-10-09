349e1d3c75e2451ffdbfe293cc37f608fd3850b6
Frank Hunleth <fhunleth@troodon-software.com>
2024-10-09 19:55:06 -0400

    Dialyxir automatically includes optional deps now

--- mix.exs
+++ mix.exs
@@ -46,9 +46,7 @@ defmodule NervesHubLink.MixProject do
 
   defp dialyzer() do
     [
-      flags: [:missing_return, :extra_return, :error_handling, :underspecs, :unmatched_returns],
-      plt_add_apps: [:atecc508a, :nerves_key, :nerves_key_pkcs11],
-      list_unused_filters: true
+      flags: [:missing_return, :extra_return, :error_handling, :underspecs, :unmatched_returns]
     ]
   end
 
