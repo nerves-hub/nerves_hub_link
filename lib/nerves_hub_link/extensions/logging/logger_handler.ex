ce3f4815e0e4ace8d1e08627621fb48653271311
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-08-18 19:08:08 +1200

    Conditionally use the `:logger_handler` behaviour

--- lib/nerves_hub_link/extensions/logging/logger_handler.ex
+++ lib/nerves_hub_link/extensions/logging/logger_handler.ex
@@ -6,13 +6,19 @@ defmodule NervesHubLink.Extensions.Logging.LoggerHandler do
   @moduledoc """
   Send logs to NervesHub.
   """
-  @behaviour :logger_handler
+  if System.otp_release() |> String.to_integer() >= 27 do
+    @behaviour :logger_handler
+  end
 
   alias NervesHubLink.Extensions.Logging
 
+  if System.otp_release() |> String.to_integer() >= 27 do
+    @impl :logger_handler
+    def log(log_event, config)
+  end
+
   # Callback for :logger handlers
   @doc false
-  @impl :logger_handler
   def log(%{msg: {:string, unicode_chardata}} = log_event, _) do
     Logging.send_log_line(log_event.level, unicode_chardata, log_event.meta)
   end
