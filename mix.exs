b9301ae72b2fe98d782e90dc56a7cd5bc78276d2
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-10-04 14:43:27 +1300

    Update version in `mix.exs`

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesHubLink.MixProject do
   use Mix.Project
 
-  @version "2.9.0-rc.3"
+  @version "2.9.0"
   @description "Manage your Nerves fleet by connecting it to NervesHub"
   @source_url "https://github.com/nerves-hub/nerves_hub_link"
 
