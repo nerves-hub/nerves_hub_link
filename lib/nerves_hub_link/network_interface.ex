ba3841dbc3b572da7eecb0a56267a5e4df0d9b81
Nate Shoemaker <nshoes@users.noreply.github.com>
2026-03-17 10:23:06 -0700

    Update lib/nerves_hub_link/network_interface.ex
    Co-authored-by: Jon Carstens <jjcarstens@me.com>

--- lib/nerves_hub_link/network_interface.ex
+++ lib/nerves_hub_link/network_interface.ex
@@ -13,7 +13,7 @@ defmodule NervesHubLink.NetworkInterface do
   Get the network interface from a Slipstream.Socket or Mint.HTTP struct. This is used to report the
   network interface being used for the connection to NervesHub and for firmware downloads.
   """
-  @spec from_socket(Slipstream.Socket.t() | Mint.Types.socket()) :: nil | binary()
+  @spec from_socket(:ssl.socket() | :inet.socket() | Mint.Types.socket()) :: nil | binary()
   def from_socket(socket) do
     case socket do
       {:sslsocket, _, _} ->
